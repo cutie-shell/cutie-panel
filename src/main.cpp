617cfa5dff7dbfa171df6d67f89747151d65cc40
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2025-11-16 07:34:03 +0200

    Migrate to upstream layer-shell-qt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <QGuiApplication>
 #include <QQuickView>
 #include <QIcon>
-#include <LayerShellQt6/shell.h>
-#include <LayerShellQt6/window.h>
+#include <LayerShellQt/shell.h>
+#include <LayerShellQt/window.h>
 
 #include "quicksettings.h"
 
